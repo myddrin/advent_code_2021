700276d41098bb5da34e0edcf4fd6051dd0c82a5
Thomas Richard <thomrich0@gmail.com>
2021-12-05 08:54:58 +0000

    Day 5 minor cleanup
    And ignore output files in git repo

--- day_05/compute.py
+++ day_05/compute.py
@@ -2,7 +2,7 @@ import dataclasses
 from typing import Dict, Iterable, List
 
 
-@dataclasses.dataclass()
+@dataclasses.dataclass(frozen=True)
 class Point:
     x: int
     y: int
@@ -15,16 +15,12 @@ class Point:
     def __str__(self):
         return f"{self.x},{self.y}"
 
-    def __hash__(self):
-        # TODO(tr) Dataclass thinks 2 ints are unsafe hash, why?
-        return hash(str(self))
+    # def __hash__(self):
+    #     # Dataclass thinks 2 ints are unsafe hash, because we're not immutable.
+    #     # Let's add frozen=True and all will be solved.
+    #     return hash(str(self))
 
     def line(self, other: "Point", straight: bool = True) -> Iterable["Point"]:
-        min_x = min(self.x, other.x)
-        max_x = max(self.x, other.x)
-        min_y = min(self.y, other.y)
-        max_y = max(self.y, other.y)
-
         step_x = 0
         step_y = 0
         if self.y != other.y:
@@ -35,7 +31,7 @@ class Point:
         if straight and step_x != 0 and step_y != 0:
             raise ValueError(f'Does not support not straight lines {str(self)}->{str(other)}')
 
-        st = Point(self.x, self.y)
+        st = self
         while st != other:
             yield st
             st = Point(st.x + step_x, st.y + step_y)
@@ -84,6 +80,12 @@ class Map:
 
         return lines
 
+    def write_map(self, filename: str):
+        print(f'Loaded map with {len(self.points)} points, writing it into "{filename}"')
+        with open(filename, 'w') as f:
+            for l in self.map_str():
+                f.write(l + '\n')
+
     @classmethod
     def load_map(cls, filename: str, straight: bool = True):
         rv = Map()
@@ -104,22 +106,15 @@ class Map:
         return rv
 
 
-def write_map(datamap: Map, filename: str):
-    print(f'Loaded map with {len(datamap.points)} points, writing it into "{filename}"')
-    with open(filename, 'w') as f:
-        for l in datamap.map_str():
-            f.write(l + '\n')
-
-
 if __name__ == '__main__':
     data = Map.load_map('input.txt', straight=True)
-    write_map(data, 'output_q1.txt')
+    data.write_map('output_q1.txt')
 
     straight = data.count_more_than(1)
     print(f'Q1: there are {straight} positions with more than 1 vent (diagonals excl.)')
 
     data = Map.load_map('input.txt', straight=False)
-    write_map(data, 'output_q2.txt')
+    data.write_map('output_q2.txt')
 
     diag = data.count_more_than(1)
     print(f'Q2: there are {diag} positions with more than 1 vent (diagonals incl.)')
