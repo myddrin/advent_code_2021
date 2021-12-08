f0644cb797fbbc19a539f75a2fe918b42fe037ed
Thomas Richard <thomrich0@gmail.com>
2021-12-08 16:21:49 +0000

    Day 8, q2
    There has to be a simpler way...

--- day_08/compute.py
+++ day_08/compute.py
@@ -1,7 +1,9 @@
 import dataclasses
 from argparse import ArgumentParser
+from copy import deepcopy
 from enum import Enum
-from typing import List, Dict
+from itertools import product
+from typing import List, Dict, Optional
 
 
 class Pattern(Enum):
@@ -60,11 +62,52 @@ b    .  b    .  .    c  b    c  b    c
         raise ValueError()
 
 
+def all_matches() -> Dict[str, List[str]]:
+    return {
+        l: ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        for l in 'abcdefg'
+    }
+
+
 @dataclasses.dataclass
 class Panel:
+    easy_patterns = (Pattern.One, Pattern.Four, Pattern.Seven, Pattern.Eight)
+    hard_patterns = (Pattern.Zero, Pattern.Two, Pattern.Three, Pattern.Five, Pattern.Six, Pattern.Nine)
+
     patterns: List[str]  # the 10 patterns seen on the pannel
     output: List[str]  # the output to translate with the patterns
-    digit_match: Dict[int, List[str]] = dataclasses.field(default_factory=dict)
+    digit_match: Dict[str, Pattern] = dataclasses.field(default_factory=dict)
+
+    segment_match: Dict[str, List[str]] = dataclasses.field(default_factory=all_matches)
+
+    output_value: Optional[int] = None
+
+    @classmethod
+    def update_segment(cls, match_dict: Dict[str, List[str]], pattern: str, value: str):
+        for l in pattern:
+            new_match = []
+            for old_match in match_dict[l]:
+                if old_match in value:
+                    new_match.append(old_match)
+            if len(new_match) == 0:
+                raise ValueError()
+            match_dict[l] = new_match
+
+            if len(new_match) == 1:
+                # remove from all entries, we have an exact match!
+                for k, v in match_dict.items():
+                    if k == l:
+                        continue  # don't remove it from its own!
+                    match_dict[k] = [l for l in v if l != new_match[0]]
+                    if len(match_dict[k]) == 0:
+                        raise ValueError()
+
+    def _add_match(self, pattern: Pattern, value: str):
+        if value not in self.digit_match:
+            self.digit_match[value] = pattern
+            # Keep only the intersection of the 2
+
+            self.update_segment(self.segment_match, pattern.value, value)
 
     def easy_match(self) -> int:
         """
@@ -72,28 +115,96 @@ class Panel:
         Add them to the digit match and count the number that were in the output for Q1.
         """
         rv = 0
-        for pattern in (Pattern.One, Pattern.Four, Pattern.Seven, Pattern.Eight):
+        for pattern in self.easy_patterns:
             for p in self.patterns:
                 if len(p) == len(pattern.value):
-                    if pattern.digit not in self.digit_match:
-                        self.digit_match[pattern.digit] = []
-                    self.digit_match[pattern.digit].append(p)
+                    self._add_match(pattern, p)
 
             for o in self.output:
                 if len(o) == len(pattern.value):
-                    if pattern.digit not in self.digit_match:
-                        self.digit_match[pattern.digit] = []
-                    self.digit_match[pattern.digit].append(o)
+                    self._add_match(pattern, o)
                     rv += 1
 
         return rv
 
     @classmethod
+    def potential(cls, size=3) -> List[List[int]]:
+        rv = []
+        entries = list(range(size))
+        for i in entries:
+            v = [i]
+            for j in entries:
+                if j not in v:
+                    v.append(j)
+                    for k in entries:
+                        if k not in v:
+                            v.append(k)
+                            rv.append([e for e in v])
+                            v.pop(-1)
+                    v.pop(-1)  # reset to 1 element
+        return rv
+
+    @classmethod
+    def hard_match(cls, patterns: List[str], init_match: Dict[str, List[str]]) -> Dict[str, Pattern]:
+        fives: List[Pattern] = [p for p in Pattern if len(p.value) == 5]
+        sixes: List[Pattern] = [p for p in Pattern if len(p.value) == 6]
+
+        size_five: List[str] = [s for s in patterns if len(s) == 5]
+        size_six: List[str] = [s for s in patterns if len(s) == 6]
+
+        # all potential match from fives -> size_five
+        potential = product(cls.potential(), cls.potential())
+
+        for p in potential:
+            current = deepcopy(init_match)
+            match = {}
+            for i_five, m_five in enumerate(p[0]):
+                match[size_five[m_five]] = fives[i_five]
+                for i_six, m_six in enumerate(p[1]):
+                    match[size_six[m_six]] = sixes[i_six]
+            try:
+                for k, v in match.items():
+                    cls.update_segment(current, v.value, k)
+            except ValueError:
+                continue  # wrong potential match
+            else:
+                # We found a valid combination!
+                return match
+
+    def _decode_output(self):
+        v = 0
+        for i, o in enumerate(reversed(self.output)):
+            if o not in self.digit_match:
+                raise ValueError(f'No translation for {o}')
+            v += self.digit_match[o].digit * pow(10, i)
+        return v
+
+    def value(self) -> int:
+        if self.output_value is not None:
+            return self.output_value
+
+        # Otherwise we have to compute it
+        self.easy_match()
+        for k, v in self.hard_match(self.patterns, self.segment_match).items():
+            self._add_match(v, k)
+
+        # We have to match all other patterns
+        self.output_value = self._decode_output()
+        return self.output_value
+
+    @classmethod
+    def sort_entries(cls, *args: str) -> List[str]:
+        return list(map(
+            lambda s: ''.join(sorted(s)),
+            args
+        ))
+
+    @classmethod
     def from_str(cls, value: str) -> "Panel":
         patterns, output = value.split(' | ')
         return cls(
-            patterns.split(' '),
-            output.split(' '),
+            cls.sort_entries(*patterns.split(' ')),
+            cls.sort_entries(*output.split(' ')),
         )
 
     @classmethod
@@ -111,6 +222,13 @@ class Panel:
             for p in data
         ))
 
+    @classmethod
+    def all_values(cls, data: List["Panel"]) -> int:
+        return sum((
+            p.value()
+            for p in data
+        ))
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -122,3 +240,6 @@ if __name__ == '__main__':
 
     q1 = Panel.count_easy_match(data)
     print(f'Q1: number of 1, 4, 7 or 8 in data: {q1}')
+
+    q2 = Panel.all_values(data)
+    print(f'Q2: sum of all values: {q2}')
--- day_08/test_compute.py
+++ day_08/test_compute.py
@@ -1,13 +1,22 @@
+from typing import List
+
 import pytest
 
-from day_08.compute import Panel
+from day_08.compute import Panel, Pattern
 
 
 class TestPannel:
     small_example = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
 
+    @classmethod
+    def make_panel(cls, patterns: List[str], output: List[str]) -> Panel:
+        return Panel(
+            Panel.sort_entries(*patterns),
+            Panel.sort_entries(*output),
+        )
+
     def test_from_str(self):
-        assert Panel.from_str(self.small_example) == Panel(
+        assert Panel.from_str(self.small_example) == self.make_panel(
             ['acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab', 'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab'],
             ['cdfeb', 'fcadb', 'cdfeb', 'cdbaf'],
         )
@@ -16,7 +25,7 @@ class TestPannel:
         data = Panel.from_file('example.txt')
         assert len(data) == 10
 
-        assert data[4] == Panel(
+        assert data[4] == self.make_panel(
             ['aecbfdg', 'fbg', 'gf', 'bafeg', 'dbefa', 'fcge', 'gcbea', 'fcaegb', 'dgceab', 'fcbdga'],
             ['gecf', 'egdcabf', 'bgf', 'bfgea'],
         )
@@ -26,10 +35,20 @@ class TestPannel:
             'be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe',
             2,
             {
-                1: ['be'],
-                8: ['cfbegad', 'fdgacbe'],
-                4: ['cgeb', 'gcbe'],
-                7: ['edb'],
+                'be': Pattern.One,
+                'abcdefg': Pattern.Eight,
+                'bceg': Pattern.Four,
+                'bde': Pattern.Seven,
+            },
+        ),
+        (
+            small_example,
+            0,
+            {
+                'abcdefg': Pattern.Eight,
+                'abd': Pattern.Seven,
+                'abef': Pattern.Four,
+                'ab': Pattern.One,
             },
         ),
     ))
@@ -38,6 +57,155 @@ class TestPannel:
         assert panel.easy_match() == exp_count
         assert panel.digit_match == exp_match
 
+    def test_add_match(self):
+        all_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+        panel = Panel.from_str(self.small_example)
+        assert panel.segment_match == {
+            'a': all_letters,
+            'b': all_letters,
+            'c': all_letters,
+            'd': all_letters,
+            'e': all_letters,
+            'f': all_letters,
+            'g': all_letters,
+        }
+
+        # panel.easy_match()
+        # assert panel.digit_match == {
+        #     'abcdefg': Pattern.Eight,
+        #     'abd': Pattern.Seven,
+        #     'abef': Pattern.Four,
+        #     'ab': Pattern.One,
+        # }
+        panel._add_match(Pattern.Eight, ''.join(all_letters))
+        assert panel.segment_match == {
+            'a': all_letters,
+            'b': all_letters,
+            'c': all_letters,
+            'd': all_letters,
+            'e': all_letters,
+            'f': all_letters,
+            'g': all_letters,
+        }
+
+        panel._add_match(Pattern.Seven, 'abd')  # 7: acf
+        assert panel.segment_match == {
+            'a': ['a', 'b', 'd'],
+            'b': all_letters,
+            'c': ['a', 'b', 'd'],
+            'd': all_letters,
+            'e': all_letters,
+            'f': ['a', 'b', 'd'],
+            'g': all_letters,
+        }
+
+        panel._add_match(Pattern.Four, 'abef')  # 4: bcdf
+        assert panel.segment_match == {
+            'a': ['a', 'b', 'd'],
+            'b': ['a', 'b', 'e', 'f'],
+            'c': ['a', 'b'],  # not d, e, f
+            'd': ['a', 'b', 'e', 'f'],
+            'e': all_letters,
+            'f': ['a', 'b'],  # not d, e, f
+            'g': all_letters,
+        }
+
+        panel._add_match(Pattern.One, 'ab')  # 1: cf
+        assert panel.segment_match == {
+            'a': ['a', 'b', 'd'],
+            'b': ['a', 'b', 'e', 'f'],
+            'c': ['a', 'b'],  # not d, e, f
+            'd': ['a', 'b', 'e', 'f'],
+            'e': all_letters,
+            'f': ['a', 'b'],  # not d, e, f
+            'g': all_letters,
+        }
+
+    def test_update_segment_invalid_assumption(self):
+        all_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        current = {
+            'a': ['a', 'b', 'd'],
+            'b': ['a', 'b', 'e', 'f'],
+            'c': ['a', 'b'],  # not d, e, f
+            'd': ['a', 'b', 'e', 'f'],
+            'e': all_letters,
+            'f': ['a', 'b'],  # not d, e, f
+            'g': all_letters,
+        }
+        # 2: acdeg
+        Panel.update_segment(current, 'a', 'cdefh')
+        assert current == {
+            'a': ['d'],
+            'b': ['a', 'b', 'e', 'f'],
+            'c': ['a', 'b'],
+            'd': ['a', 'b', 'e', 'f'],
+            'e': ['a', 'b', 'c', 'e', 'f', 'g'],
+            'f': ['a', 'b'],
+            'g': ['a', 'b', 'c', 'e', 'f', 'g'],
+        }
+        with pytest.raises(ValueError):
+            Panel.update_segment(current, 'c', 'cdefh')
+
+    def test_potential(self):
+        assert Panel.potential(3) == [
+            [0, 1, 2],
+            [0, 2, 1],
+            [1, 0, 2],
+            [1, 2, 0],
+            [2, 0, 1],
+            [2, 1, 0],
+        ]
+
+    def test_hard_match(self):
+        panel = Panel.from_str(self.small_example)
+
+        panel.easy_match()
+        rv = panel.hard_match(panel.patterns, panel.segment_match)
+        assert rv is not None
+        assert len(rv) == 6
+        expected = {
+            # 'ab': Pattern.One,
+            'acdfg': Pattern.Two,
+            'abcdf': Pattern.Three,
+            # 'abef': Pattern.Four,
+            'bcdef': Pattern.Five,
+            'bcdefg': Pattern.Six,
+            # 'bad': Pattern.Seven,
+            # 'abcdefg': Pattern.Eight,
+            'abcdef': Pattern.Nine,
+            'abcdeg': Pattern.Zero,
+        }
+        for k, v in expected.items():
+            assert k in rv, f'Not in {rv.keys()}'
+            assert rv[k] == v
+
+    @pytest.mark.parametrize('value, exp', (
+        (small_example, 5353),
+        ('be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe', 8394),
+    ))
+    def test_value(self, value, exp):
+        panel = Panel.from_str(value)
+        assert panel.value() == exp
+
+    def test_value_from_example(self):
+        data = Panel.from_file('example.txt')
+        assert [
+            d.value()
+            for d in data
+        ] == [
+            8394,
+            9781,
+            1197,
+            9361,
+            4873,
+            8418,
+            4548,
+            1625,
+            8717,
+            4315,
+        ]
+
 
 def test_example_q1():
     assert Panel.count_easy_match(Panel.from_file('example.txt')) == 26
@@ -45,3 +213,11 @@ def test_example_q1():
 
 def test_q1():
     assert Panel.count_easy_match(Panel.from_file('input.txt')) == 367
+
+
+def test_example_q2():
+    assert Panel.all_values(Panel.from_file('example.txt')) == 61229
+
+
+def test_q2():
+    assert Panel.all_values(Panel.from_file('input.txt')) == 974512
